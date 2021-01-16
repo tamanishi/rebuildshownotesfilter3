4aa567e550eab2b2a52bcb5859ec5d8783f23ec1
Masayuki Sunahara <tamanishi@gmail.com>
2021-01-16 11:53:54 +0900

    unescape

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class Episode extends Component {
   render() {
     const episode = this.props.episode;
     return (<p>
-      <span className="epititle"><a href={episode.mediaUrl} target='_blank' rel='noopener noreferrer'>{episode.title}</a></span> <span className="pubdate">(<Moment format='YYYY/MM/DD'>{episode.publicationDate}</Moment>)</span>
+      <span className="epititle"><a href={episode.mediaUrl} target='_blank' rel='noopener noreferrer'>{unescape(episode.title)}</a></span> <span className="pubdate">(<Moment format='YYYY/MM/DD'>{episode.publicationDate}</Moment>)</span>
       <span>
         {episode.shownotes.map((shownote, i) => <Shownote shownote={shownote} key={i} />)}
       </span>
